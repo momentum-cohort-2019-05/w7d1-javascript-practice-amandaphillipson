e4e1a120e0cc19cc9f95b116b04ea25feb8b55b0
Amanda Phillipson <amanda.minton@gmail.com>
2019-07-09 09:34:18 -0400

    Made it through 12

--- homework.js
+++ homework.js
@@ -1,26 +1,60 @@
 // 1. Set the variable `givenName` to the string "Addison".
 
+const givenName = 'Addison';
+
 // 2. Set candies equal to 20, people to 6, and leftover equal
 // to the remainder of dividing 20 by 6.
 
+const candies = 20;
+const people = 6;
+const leftover = candies % people;
+
 // 3. Create a function called greeting that returns "Hello, <name>!",
 // where <name> is the name given as an argument to the function.
+
+function greeting(name){
+    return `Hello, ${name}!`
+}
 
 // 4. Create a function called isOdd that, given a number, will
 // return true if it is odd and false if it is not. An odd number is a
 // number which, when divided by 2, has a remainder of 1 or -1.
 
+function isOdd(number){
+    if (number % 2 === 1 || number % 2 === -1){
+        return true
+    }else {
+        return false
+    }
+}
+
 // 5. Create a function called isEven that, given a number, will
 // return true if it is even and false if it is not. An even number is a
 // number which, when divided by 2, has a remainder of 0.
+
+function isEven(number){
+    if (number % 2 === 0){
+        return true
+    }else {
+        return false
+    }
+}
 
 // 6. Create a function called fahrenheitToCelsius that takes a
 // Fahrenheit temperature as an argument and returns the
 // temperature in Celsius.
 
+function fahrenheitToCelsius(temperature){
+    return (temperature - 32) * 5 / 9
+}
+
 // 6. Create a function called celsiusToFahrenheit that takes a
 // Celsius temperature as an argument and returns the
 // temperature in Fahrenheit.
+
+function celsiusToFahrenheit(temperature){
+    return (temperature * 9 / 5) + 32
+}
 
 // 7. Create a function called fahrenheitToKelvin that takes a
 // Fahrenheit temperature as an argument and returns the
@@ -29,9 +63,21 @@
 // Absolute zero (0 K) is equivalent to −273.15 C.
 // 1 degree Kelvin equals 1 degree Celsius.
 
+function fahrenheitToKelvin(temperature){
+    return fahrenheitToCelsius(temperature) + 273.15
+}
+
 // 8. Create a function called lesser that takes two numbers as
 // arguments and returns the lesser of them. This function should
 // use an if/else statement.
+
+function lesser(num1,num2) {
+    if (num1 < num2) {
+        return num1
+    } else {
+        return num2
+    }
+}
 
 // 9. Create a function called multigreeting that takes a name
 // and a language code and returns a version of "Hello, <name>!"
@@ -45,20 +91,53 @@
 //
 // If any other language code is used, return nothing.
 
+function multigreeting(name, language) {
+    if (language === 'en'){
+        return `Hello, ${name}!`
+    } else if (language === 'es') {
+        return `¡Hola, ${name}!`
+    } else if (language === 'fr') {
+        return `Bonjour, ${name}!`
+    } else if (language === 'eo') {
+        return `Saluton, ${name}!`
+    } else {
+    }
+ }
+
 // 10. Create a function called "sum" that takes an array of numbers and
 // returns the sum of those numbers.
+
+function sum(array){
+    return array.reduce (function (x,y) {
+        return x + y 
+    }, 0);
+}
+
 
 // 11. Create a function called "average" that takes an array of numbers
 // and returns the average of those numbers.
 
+function average(array){
+    if (array.length !== 0){
+        return sum(array) / array.length
+    }
+}
+
+
 // 12. Create a function called "minimum" that takes an array of numbers and
 // returns the smallest number in that array.
 
+function minimum(array){
+    if (array.length !== 0){
+        return Math.min(...array)
+    }
+}
+
 // 13. There are many techniques to sort arrays in programming. Your programming
-// language will likely include the ability to do this. We are going to
+// language will likely include the abity to do this. We are going to
 // implement sorting ourselves, however.
 //
-// A "selection sort" is one of the most simple sorting algorithms. To implement it,
+// A "selection sort" is one of the most simple song algorithms. To implement it,
 // you start with an unsorted array of numbers. You search the array and find the
 // smallest number in the array. You then insert that into a new array as the first
 // element and remove it from the original array. You continue doing this until
@@ -77,3 +156,4 @@
 // Note 2: Selection sort can be implemented using one array. Read the explanation at
 // https://courses.cs.vt.edu/csonline/Algorithms/Lessons/SelectionSort/index.html
 // to see how. This may make more sense to you.
+
